d0637e52fa42bcc9508512fdef06bac1a9d309f6
Yegor Stephanovich <stefanovich_egor@mail.ru>
2018-12-27 10:51:53 +0300

    Add table header, start work with books

--- js/httpRequests.js
+++ js/httpRequests.js
@@ -1,6 +1,7 @@
 let path = 'http://samples.databoom.space/api1/sampledb/collections/';
 //let path = 'https://samples.databoom.space/api1/sampledb/collections/persons?$filter=firstname eq \'Lamar\'';
-let arr = [];
+let personList = [];
+let bookList = [];
 
 
 window.onload = function () {
@@ -22,20 +23,36 @@ function setConfig(path) {
 function getData(page, filter) {
     o(page).where(filter).get(function(data) {
         for (let i = 0; i < data.d.results.length; i++){
-            arr.push(new Person(data.d.results[i]));
+            personList.push(new Person(data.d.results[i]));
         }
+        console.log(bookList);
 
-        toGrid(arr);
+        if (personList !== undefined)
+            toGrid(personList);
     });
 }
 
-function toGrid(arr) {
+function toGrid(personList) {
     let table = document.createElement('table');
 
-    for (let elem in  arr)
-        table.appendChild(createRow(arr[elem]));
+   table.appendChild(createHeadRow(personList[0]));
+
+    for (let elem in  personList)
+        table.appendChild(createRow(personList[elem]));
 
     document.body.appendChild(table);
+}
+
+function createHeadRow(elem) {
+    let row = document.createElement('tr');
+
+    for (let key in elem) {
+        let cell = document.createElement('th');
+        cell.innerText = key;
+        row.appendChild(cell);
+    }
+
+    return row;
 }
 
 function createRow(elem) {
@@ -56,15 +73,20 @@ function Person(person) {
     this.firstname = person.firstname;
     this.lastname = person.lastname;
     this.age = person.age;
-    this.collections = person.collections;
+   // this.collections = person.collections;
     this.likes = person.likes;
+    for (let bookId in person.likes){
+        if (bookList.indexOf(person.likes[bookId]) === -1){
+            bookList.push(person.likes[bookId]);
+        }
+    }
 }
 
 //constructor for book
 function Book(book) {
     this.id = book.id;
     this.title = book.title;
-    this.collections = book.collections;
+    //this.collections = book.collections;
     this.author = book.author;
     this.publisher = book.publisher;
 }
